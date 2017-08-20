7483cfed7ae222245852230b84d3b4a8a9ccbb91
abelsuranyi <abel.suranyi@balabit.com>
2017-08-20 12:25:21 +0200

    added create to Entity

--- scribe/Entity.h
+++ scribe/Entity.h
@@ -1,14 +1,24 @@
 #ifndef SCRIBE_ENTITY_H_INCLUDED
 #define SCRIBE_ENTITY_H_INCLUDED
 
+#include <memory>
+
 namespace scribe
 {
     class EntityProcessor;
     class Entity
     {
         public:
+            virtual ~Entity() = default;
+
             virtual void processBy(EntityProcessor& processor) = 0;
             virtual void processBy(EntityProcessor& processor) const = 0;
+
+            template <typename T, typename... Args>
+            static std::unique_ptr<Entity> create(Args&&... args)
+            {
+                return std::unique_ptr<Entity>(new T(std::forward<Args...>(args)...));
+            }
     };
 }
 
